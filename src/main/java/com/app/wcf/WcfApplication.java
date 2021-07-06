01f5c9c5f5a6ae6cefeb7dc655c7e3b45b0ef202
jacob <jisaacs46@gmail.com>
2021-07-06 17:16:02 -0600

    first commit

---
+++ src/main/java/com/app/wcf/WcfApplication.java
@@ -0,0 +1,13 @@
+package com.app.wcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/wcf/WcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
